df9b48b556bc2cf5ce84a26612be565fe520233c
Mehdi <mehdi-bs@live.fr>
2019-03-20 11:26:38 +0100

    Update Code.R

--- Code.R
+++ Code.R
@@ -1,12 +1,14 @@
-setwd("../Serie temporelles/projet")
+
+setwd("../projet")
 
 #Requirements
 library(readxl)
 library(zoo)
+library(tseries)
 
 
 #Chargement des données
-data <- read_excel("D:/Documents/ensae cours/S2/Serie temporelles/projet/series_longues_ipi_201712 (1).xls")
+data <- read_excel("../series_longues_ipi_201712 (1).xls")
 
 #Nettoyage et formats
 st <- data[,c("NAF rev. 2","(C2)")]
@@ -18,8 +20,31 @@ date <- function(x){
 }
 
 st$Date <- lapply(st$Date,date)
-st$Datef <- lapply(st$Date,as.Date)
+st[['Date']] <- strptime(st[['Date']], format='%Y-%m-%d')
 
 
 #plot de la série temp
-plot.ts(st$valeur)
+plot(st$valeur ~ as.Date(st$Date),type="l",xlab="Temps",ylab = "S_t")
+
+
+#Question 2
+#La série n'a pas l'air d'être stationnaire à l'oeil.
+#vérifions d'abord par un acf (non formel) puis par
+# un test de Dickey Fuller: 
+
+#acf
+acf(st$valeur, lag.max=334) #oulala !! c'est pas très bon 
+
+#DF
+adf.test(st$valeur,k=6) #on ne rejette pas la non stationnarité
+
+
+#tranformation pour rendre stationnaire.
+Dt<-diff(st$valeur) #diff à l'ordre 1
+plot(Dt ~ as.Date(st$Date)[2:length(st$Date)],type="l",xlab="Temps",ylab = "D_t",col="orange") #top
+
+
+acf(Dt) #bizarre 
+adf.test(Dt,k=0) #ça c'est ok pour k=0,1,2,3,4,5,6
+
+#Question 3
